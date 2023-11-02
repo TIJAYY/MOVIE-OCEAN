e941c51895dcb71f7af02c2e3d60ebd4fc227be8
Tushar Jangra <that.tijay@gmail.com>
2023-11-03 04:01:56 +0530

    small changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/movix-logo.png";
 
 const Header = () => {
     const [show, setShow] = useState("top");
@@ -75,6 +75,9 @@ const Header = () => {
             <ContentWrapper>
                 <div className="logo" onClick={() => navigate("/")}>
                     <img src={logo} alt="" />
+                     <span className="headerText">
+                        MovieOcean
+                     </span>
                 </div>
                 <ul className="menuItems">
                     <li
